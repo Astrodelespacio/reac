db6771554393eaaca0990eda128901382aecd1d3
Astrodelespacio <137854304+Astrodelespacio@users.noreply.github.com>
2023-06-29 18:49:28 -0600

    Add files via upload

---
+++ Ipie.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Ipie.css'
+
+const Ipie = () =>{
+    return(
+        <div className="pie">
+          <h1>Pie de pagina</h1>
+
+         
+        </div>
+    )
+}
+
+export default Ipie
