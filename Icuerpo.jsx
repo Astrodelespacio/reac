e51e24f83e17188efc3a40a05f1aa83fc5b697d4
Astrodelespacio <137854304+Astrodelespacio@users.noreply.github.com>
2023-06-29 18:48:34 -0600

    Add files via upload

---
+++ Icuerpo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Icuerpo.css'
+
+const Icuerpo = () =>{
+    return(
+        <div className="cuerpo">
+          <h1 className="titulo">Bienvenidos a esta seccion</h1>
+          <p className="nota">Este es el cuerpo de doc</p>
+         
+        </div>
+    )
+}
+
+export default Icuerpo
